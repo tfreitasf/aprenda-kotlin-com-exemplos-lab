9d9d700a94393a66e12b810bfd591a530c9d6c72
Thiago Mauricio de Freitas <83042767+tfreitasf@users.noreply.github.com>
2022-12-31 01:44:23 -0300

    refatoração classe formacao

--- desafio.kt
+++ desafio.kt
@@ -9,11 +9,18 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(Usuario)
+    }
+    
+    fun imprimirConteudoEducacional(){
+        for (i in conteudos){
+            println("${i.nome} - ${i.duracao}")
+        }
     }
 }
 
 fun main() {
+    
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
