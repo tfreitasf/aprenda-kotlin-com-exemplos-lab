f64081e8d97df71b6842a9a537186e60e72c0f04
Thiago Mauricio de Freitas <83042767+tfreitasf@users.noreply.github.com>
2022-12-31 02:03:08 -0300

    inicio dos testes

--- desafio.kt
+++ desafio.kt
@@ -21,6 +21,19 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 
 fun main() {
     
+    val formaçãoAndroidDeveloper = ConteudoEducacional("Formação Android Developer", 67, Nivel.INTERMEDIARIO)
+    val formaçãoFlutterSpecialist = ConteudoEducacional("Formação Flutter Specialist", 71, Nivel.INTERMEDIARIO)
+    val formaçãoGoogleCloudPlatformt = ConteudoEducacional("Formação Google Cloud Platform", 58, Nivel.AVANCADO)
+    val formaçãoLinuxFundamentals = ConteudoEducacional("Formação Linux Fundamentals", 23, Nivel.BASICO)
+    
+    val cleitonFernandes = Usuario("Cleiton Fernandes", "Dio Free", "cleiton.fernandes@gmail.com")
+    val jussaraSilva = Usuario("Jussara Silva", "Dio Global", "jussara.silva@gmail.com")
+    val genilsonTome = Usuario("Genilson Tome", "Dio Pro", "genilson.tome@gmail.com")
+    
+    formaçãoAndroidDeveloper.matricular(cleitonFernandes)
+    
+    
+    
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
